4794d6335ea9202da642bca622bb89523f23681a
Urvatov <urvatov@gmail.com>
2024-02-26 16:36:47 +0300

    +create_table.py

---
+++ create_table.py
@@ -0,0 +1,16 @@
+import sqlite3
+
+def create_table():
+    connection = sqlite3.connect("database.db")
+    cursor = connection.cursor()
+    
+    cursor.execute("""CREATE TABLE IF NOT EXISTS employees 
+                   (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    name TEXT NOT NULL,
+                    date_of_birth DATE NOT NULL,
+                    sex TEXT NOT NULL
+                   )""")
+    
+    print("Таблица создана")
+    
