724b4b4e1a5850b4a9f7006d206be93b64e55434
Urvatov <urvatov@gmail.com>
2024-02-26 16:49:34 +0300

    +class Employee

---
+++ Employee.py
@@ -0,0 +1,18 @@
+from datetime import date
+from main import cursor, connection
+
+class Employee:
+    def __init__(self, name : str, date_of_birth : date, sex : str):
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.sex = sex
+    
+    def get_age(self) -> int:
+        return date.today().year - self.date_of_birth.year
+    
+    def save_to_database(self):
+        cursor.execute("""INSERT INTO employees (name, date_of_birth, sex) 
+                        VALUES (?, ?, ?)""", (self.name, self.date_of_birth, self.sex))
+        
+        connection.commit()
+        connection.close()
--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import sys
+import sqlite3
 
 from create_table import create_table
 
 def main(arg : int):
     print(f"Аргумент: {arg}")
 
+    global connection
+    connection = sqlite3.connect()
+
+    global cursor
+    cursor = connection.cursor()
+    
     match arg:
         case 1: create_table()
 
